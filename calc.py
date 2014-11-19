e70b01bc05dd35200f13fc8f12026ce31c9ac816
Conlin M. Durbin <durbinc@xinu18.cs.purdue.edu>
2014-11-19 11:52:11 -0500

    Added Modulo operator

--- calc.py
+++ calc.py
@@ -33,6 +33,13 @@ def mult(a, b):
 def div(a, b):
     return a / b
 
+# Modulus Division function
+# a -- dividend
+# b -- divisor
+def mod(a, b):
+    return a % b
+
+
 
 # -------------------------------------------------------- #
 
@@ -67,6 +74,8 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
+        elif (op == "%"):
+            print "Modulous: ", mod(a, b)
         else:
             print "Invalid operation..."
 
